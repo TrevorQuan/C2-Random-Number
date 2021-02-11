d3b0b6c96c7aec31995cdf9b4a6203c320dc6a19
Quan Trevor <124255@eesd.org>
2021-02-11 23:05:20 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,32 @@
+import random
+
+print("Coin Flip")
+
+# 1 = heads, 2 = tails
+coin = random.randint(1,2)
+
+if (coin == 1):
+  print("It's Heads!")
+else:
+  print("It's Tails!")
+
+print()
+#Exercise: Create a code that mimics a dice roll using random numbers. The code should print out what the number is "rolled", letting the user know what the result is.
+
+import random
+
+print("Dice Roll")
+
+dice = random.randint(1,6)
+if(dice == 1):
+  print("You rolled a 1!")
+elif(dice == 2):
+  print("You rolled a 2!")
+elif(dice == 3):
+  print("You rolled a 3!")
+elif(dice == 4):
+  print("You rolled a 4!")
+elif(dice == 5):
+  print("You rolled a 5!")
+elif(dice == 6):
+  print("You rolled a 6!")
